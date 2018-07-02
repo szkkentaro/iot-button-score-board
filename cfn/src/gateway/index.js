eab626e7680199437f2190722bd0501dee41663a
Kentaro SUZUKI <kentarock@gmail.com>
2018-07-03 05:33:19 +0900

    fix: Change id format

--- cfn/src/gateway/index.js
+++ cfn/src/gateway/index.js
@@ -2,14 +2,9 @@ const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB();
 
 exports.handler = async (event, context) => {
-    const id = [
-        event.placementInfo.attributes.team,
-        event.deviceInfo.deviceId
-    ].join("-");
-
     await dynamo.updateItem({
         TableName: process.env.DYNAMODB_TABLE,
-        Key: { Id: { S: id } },
+        Key: { Id: { S: `id-${event.placementInfo.attributes.id}` } },
         AttributeUpdates: { Score: { Action: 'ADD', Value: { N: "1" } } },
         ReturnValues: 'ALL_NEW'
     }).promise().catch(console.err);
